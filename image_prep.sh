6ff9c32dfe7e54d71a99d4f5f7c89959ac49f5f6
Paul Creaser <PaulCreaser@users.noreply.github.com>
2016-02-21 14:39:25 +0900

    Script for Image text file preparation

---
+++ image_prep.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Command line contains $# arguments"
+else
+    echo "Required arguments"
+    echo "$0 1:[image_dir] 2:[output_filename]"
+    exit
+fi
+
+out_file=$2
+echo $out_file
+
+rm $out_file
+
+rel_path=$1
+full_path=$(readlink -f $rel_path)
+echo $full_path
+
+for file in "$full_path"/*
+do
+    if [[ -f $file ]]; then
+        # Format file name, label
+        # Label is set to 1, you need to manually set this :(
+        line="$file,1"
+        echo $line >> $out_file
+    fi
+done
