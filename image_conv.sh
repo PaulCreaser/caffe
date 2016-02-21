a68f7826b70638ae0f379f53dc399d0198faf783
Paul Creaser <PaulCreaser@users.noreply.github.com>
2016-02-21 18:41:23 +0900

    Add necessary file name formatting for caffe

--- image_conv.sh
+++ image_conv.sh
@@ -19,21 +19,29 @@ echo $out_full_path
 mkdir -p $out_full_path
 rm $out_full_path/*
 
+COUNTER=0
+echo " $COUNTER "
+
 for file in "$full_path"/*
 do
     if [[ -f $file ]]; then
-        # Format file name, label
-        # Label is set to 1, you need to manually set this :(
+	# Format file name, label
+	# Label is set to 1, you need to manually set this :(
 
-        basefile="${file##*/}"
-        file_noext="${basefile%.*}"
-        file_jpg="$file_noext.jpg"
-        full_file_jpg="$out_full_path/$file_jpg"
-        echo $full_file_jpg
-        if [[ -f $file_jpg ]]; then
-                rm $file_jpg
-        fi
-        #convert $file $file_jpg -size 128 128
-        convert $file $full_file_jpg
+	echo "-------------------------------->>>>"
+	basefile="${file##*/}"
+	file_noext="${basefile%.*}"
+	doe="_"
+	file_cnt="$file_noext$doe$COUNTER"
+	echo $file_cnt
+	file_jpg="$file_cnt.jpeg"
+	full_file_jpg="$out_full_path/$file_jpg"
+	echo $full_file_jpg
+	if [[ -f $full_file_jpg ]]; then
+		rm $file_jpg
+	fi
+	convert $file $full_file_jpg
+	COUNTER=$[$COUNTER +1]
+	echo "<<<<--------------------------------"
     fi
 done
